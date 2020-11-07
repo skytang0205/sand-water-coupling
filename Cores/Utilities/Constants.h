9704e2a6e1639ff42d27fea692dd3ca58a429b03
Univstar <nixy@qq.com>
2020-11-07 11:51:57 +0800

    add Constants.h

---
+++ Cores/Utilities/Constants.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <numbers>
+
+namespace PhysX {
+
+const double kGravity = 9.8;
+const double kGravitational = 6.67259e-11;
+
+const double kVacuumPermittivity = 8.854187817e-12;
+const double kVacuumPermeability = std::numbers::pi * 4e-7;
+const double kLightSpeed = 299792458;
+
+const double kBoltzmann = 1.380649e-23;
+
+}
