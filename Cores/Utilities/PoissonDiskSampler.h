02aa3aca547c657b14eb6cf62512e77db21d2843
skytang0205 <skytang0205@outlook.com>
2024-09-13 20:01:37 +0800

    add Poisson disk sample

---
+++ Cores/Utilities/PoissonDiskSampler.h
@@ -0,0 +1,191 @@
+#pragma once
+
+#include <vector>
+#include <numbers>
+#include <random>
+#include <cmath>
+#include <algorithm>
+#include "Utilities/Types.h"
+#include "Structures/ParticlesBasedData.h"
+#include "Structures/ParticlesAttribute.h"
+#include "Structures/StaggeredGrid.h"
+
+namespace PhysX {
+    template<int Dim> class PoissonDiskSampler{
+        DECLARE_DIM_TYPES(Dim)
+    public:
+        PoissonDiskSampler(VectorDr boundary, VectorDr center, real radius)
+            : boundary(boundary), center(center),
+            up_bound(center + boundary), low_bound(center - boundary),
+            radius(radius) {
+            gridCellSize = radius / std::sqrt(Dim);
+            grid_size = ((up_bound-low_bound) / gridCellSize).cast<int>();
+            int size = 1;
+            for(int i = 0; i < Dim; i++)
+                size *= grid_size(i);
+            grid.resize(size, -1);
+        }
+
+        VectorDr generateNewPoint(VectorDr point){
+            static std::random_device rd;
+            static std::mt19937 gen(rd());
+            static std::uniform_real_distribution<> disX(low_bound(0), up_bound(0));
+            static std::uniform_real_distribution<> disY(low_bound(1), up_bound(1));
+            static std::uniform_real_distribution<> disZ(low_bound(2), up_bound(2));
+            static std::uniform_real_distribution<> dis(0.0, 1.0);
+
+            if constexpr (Dim == 2){
+                if (point == VectorDr::Zero()){
+                    VectorDr initialPoint = VectorDr::Ones();
+                    initialPoint(0) = disX(gen);
+                    initialPoint(1) = disY(gen);
+                    return initialPoint;
+                }
+                real angle = dis(gen) * 2 * std::numbers::pi;
+                real dist = radius + dis(gen) * radius;
+                VectorDr newPoint = {std::cos(angle), std::sin(angle)};
+                newPoint = newPoint * dist + point;
+                return newPoint;
+            }
+            else if constexpr (Dim == 3){      
+                if (point == VectorDr::Zero()){
+                    VectorDr initialPoint = VectorDr::Ones();
+                    initialPoint(0) = disX(gen);
+                    initialPoint(1) = disY(gen);
+                    initialPoint(2) = disZ(gen);
+                    return initialPoint;
+                }          
+                real angle1 = disX(gen) * 2 * std::numbers::pi;
+                real angle2 = std::acos(1 - 2 * disX(gen));
+                real dist = radius + disX(gen) * radius * 0.5;
+                Vector<3, real> newPoint = {std::cos(angle1) * std::sin(angle2),
+                                    std::sin(angle1) * std::sin(angle2),
+                                    std::cos(angle2)};
+
+                newPoint = newPoint * dist + point;
+                return newPoint;
+                }
+            }
+
+
+        
+
+        std::vector<VectorDr> generatePoints(){
+            std::vector<VectorDr> samples;
+            std::vector<VectorDr> processList;
+            VectorDr initialPoint = generateNewPoint(VectorDr::Zero());
+            processList.push_back(initialPoint);
+            addToGrid(initialPoint);
+            static std::random_device rd;
+            static std::mt19937 gen(rd());
+
+            while (!processList.empty()) {
+                static std::uniform_int_distribution<> disI(0, processList.size() - 1);
+                int idx = disI(gen);
+                VectorDr point = processList[idx];
+                bool found = false;
+
+                for (int i = 0; i < 300; ++i) {
+                    VectorDr newPoint = generateNewPoint(point);
+                    if (isInBounds(newPoint) && isFarEnough(newPoint)) {
+                        //samples.push_back(newPoint);
+                        processList.push_back(newPoint);
+                        addToGrid(newPoint);
+                        found = true;
+                        break;
+                    }
+                    // if(!isInBounds(newPoint)) printf("not in bounds\n");
+                    // if (!isFarEnough(newPoint)) printf("not far\n");
+                }
+                if (!found) {
+                    processList.erase(processList.begin() + idx);
+                    //printf("yes\n");
+                }
+                printf("%zd %zd %zd\n",grid.size(),samples.size(), processList.size());
+                if(gridPoints.size()>grid.size()*1.1) break;
+            }
+            
+
+            for (const auto & idx : grid){
+                if (idx != -1){
+                    samples.push_back(gridPoints[idx]);
+                }
+            }
+            return samples;
+        }
+
+        bool isInBounds(const VectorDr& point) const {
+            VectorDi grid_idx = ((point - low_bound )/ gridCellSize).cast<int>();
+            if constexpr(Dim == 2)
+                return grid_idx.x() >= 0 && grid_idx.x() < grid_size.x() && grid_idx.y() >= 0 && grid_idx.y() < grid_size.y();
+            else if constexpr (Dim == 3)
+                return grid_idx.x() >= 0 && grid_idx.x() < grid_size.x() && grid_idx.y() >= 0 && grid_idx.y() < grid_size.y() && grid_idx.z() >= 0 && grid_idx.z() < grid_size.z();
+        }
+
+    private:
+        real radius, gridCellSize;
+        VectorDr boundary, center, low_bound, up_bound;
+        VectorDi grid_size;
+        std::vector<int> grid;
+        std::vector<VectorDr> gridPoints;
+        
+
+        
+        
+
+        bool isFarEnough(const VectorDr& point) const {
+            VectorDi grid_idx = ((point - low_bound )/ gridCellSize).cast<int>();
+
+            // for (const auto & p : gridPoints)
+            //     if ((p - point).norm() < radius)
+            //         return false;
+            // return true;
+
+            if constexpr (Dim == 2){
+                for (int y = std::max(0, grid_idx.y() - 3); y <= std::min(grid_size.y() - 1, grid_idx.y() + 3); ++y) {
+                    for (int x = std::max(0, grid_idx.x() - 3); x <= std::min(grid_size.x() - 1, grid_idx.x() + 3); ++x) {
+                        int i = y * grid_size.x() + x;
+                        int idx = grid[i];
+                        if (idx != -1) {
+                            VectorDr p = gridPoints[idx];
+                            if ((p-point).norm() < radius) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+                return true;
+            }
+            else if constexpr (Dim == 3){
+                for (int z = std::max(0, grid_idx.z() - 2); z <= std::min(grid_size.z() - 1, grid_idx.z() + 2); ++z){
+                    for (int y = std::max(0, grid_idx.y() - 2); y <= std::min(grid_size.y() - 1, grid_idx.y() + 2); ++y) {
+                        for (int x = std::max(0, grid_idx.x() - 2); x <= std::min(grid_size.x() - 1, grid_idx.x() + 2); ++x) {
+                            int idx = grid[z * grid_size.y() * grid_size.x() + y * grid_size.x() + x];
+                            if (idx != -1) {
+                                VectorDr p = gridPoints[idx];
+                                if ((p-point).norm() < radius) {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
+                return true;
+            }
+
+            return false;
+        }
+
+        void addToGrid(const VectorDr& point) {
+            VectorDi grid_idx = ((point - low_bound) / gridCellSize).cast<int>();
+            if constexpr (Dim == 2){
+                grid[grid_idx.y() * grid_size.x() + grid_idx.x()] = gridPoints.size();
+                gridPoints.push_back(point);
+            }
+            else if constexpr (Dim == 3){
+                grid[grid_idx.z() * grid_size.y() * grid_size.x() + grid_idx.y() * grid_size.x() + grid_idx.x()] = gridPoints.size();
+                gridPoints.push_back(point);
+            }
+        }
+    };
+}
--- Cores/Utilities/Shapes.h
+++ Cores/Utilities/Shapes.h
@@ -3,7 +3,9 @@
 #include <algorithm>
 #include <numeric>
 #include <vector>
+#include <random>
 #include "Utilities/Types.h"
+#include "Utilities/PoissonDiskSampler.h"
 #include "Structures/ParticlesBasedData.h"
 #include "Structures/ParticlesAttribute.h"
 #include "Structures/StaggeredGrid.h"
@@ -27,32 +29,66 @@ namespace PhysX {
         }
         virtual ~Shapes() = default;
 
-        void generateBox(const VectorDr center = VectorDr::Zero(), const VectorDr & halfLengths = VectorDr::Ones()) {
-            if constexpr (Dim == 2) {
-                const real spacing = _radius * 2;
-                const int  xScale  = int(std::round(halfLengths[0] * 2 / spacing));
-                const int  yScale =
-                    int(std::round((halfLengths[1] * 2 / spacing - 1) * 2 * real(std::numbers::inv_sqrt3) + 1));
-                const VectorDr origin =
-                    center - VectorDr(xScale - 1, (yScale - real(1)) * real(std::numbers::sqrt3) / 2) * spacing / 2;
-                for (int j = 0; j < yScale; j++) {
-                    const VectorDr yOrigin =
-                        origin + VectorDr(j & 1 ? real(.5) : real(0), j * real(std::numbers::sqrt3) / 2) * spacing;
-                    for (int i = 0; i < xScale - (j & 1); i++)
-                        positions._data.push_back(yOrigin + VectorDr::Unit(0) * i * spacing);
+        void generateBox(const VectorDr center = VectorDr::Zero(), const VectorDr & halfLengths = VectorDr::Ones(), bool if_Possion = false) {
+            if (!if_Possion){
+                if constexpr (Dim == 2) {
+                    const real spacing = _radius * 2;
+                    const int  xScale  = int(std::round(halfLengths[0] * 2 / spacing));
+                    const int  yScale =
+                        int(std::round((halfLengths[1] * 2 / spacing - 1) * 2 * real(std::numbers::inv_sqrt3) + 1));
+                    const VectorDr origin =
+                        center - VectorDr(xScale - 1, (yScale - real(1)) * real(std::numbers::sqrt3) / 2) * spacing / 2;
+                    for (int j = 0; j < yScale; j++) {
+                        const VectorDr yOrigin =
+                            origin + VectorDr(j & 1 ? real(.5) : real(0), j * real(std::numbers::sqrt3) / 2) * spacing;
+                        for (int i = 0; i < xScale - (j & 1); i++)
+                            positions._data.push_back(yOrigin + VectorDr::Unit(0) * i * spacing);
+                    }
+                } else {
+                    const real     spacing    = 2 * real(std::numbers::sqrt2) * _radius;
+                    const VectorDi resolution = ((halfLengths - VectorDr::Ones() * _radius) * 2 / spacing)
+                                                    .array()
+                                                    .round()
+                                                    .template cast<int>()
+                                                    .matrix();
+                    StaggeredGrid<Dim> grid(0, spacing, resolution, center);
+                    grid.forEachFace(
+                        [&](const int axis, const VectorDi & face) { positions._data.push_back(grid.faceCenter(axis, face)); });
+                    grid.forEachNode([&](const VectorDi & node) { positions._data.push_back(grid.nodeCenter(node)); });
                 }
-            } else {
-                const real     spacing    = 2 * real(std::numbers::sqrt2) * _radius;
-                const VectorDi resolution = ((halfLengths - VectorDr::Ones() * _radius) * 2 / spacing)
-                                                .array()
-                                                .round()
-                                                .template cast<int>()
-                                                .matrix();
-                StaggeredGrid<Dim> grid(0, spacing, resolution, center);
-                grid.forEachFace(
-                    [&](const int axis, const VectorDi & face) { positions._data.push_back(grid.faceCenter(axis, face)); });
-                grid.forEachNode([&](const VectorDi & node) { positions._data.push_back(grid.nodeCenter(node)); });
             }
+            else {
+                // static std::random_device rd;
+                // static std::mt19937 engine(rd());
+                // static std::uniform_real_distribution dis(-1., 1.);
+
+                // auto check_inside = [&](VectorDr pos) {
+                //     for (const auto & p : positions._data)
+                //         if ((p - pos).norm() < 2 * _radius)
+                //             return true;
+                //     return false;
+                // };
+
+                // if constexpr (Dim == 2) {
+                //     int fail = 0;
+
+                //     while (fail < 20000){
+                //         double x = dis(engine), y = dis(engine);
+                //         VectorDr rp = x * VectorDr::Unit(0) + y * VectorDr::Unit(1);
+                //         VectorDr pos = center + rp * halfLengths.x();
+
+                //         if (check_inside(pos))
+                //             ++fail;
+                //         else
+                //             positions._data.push_back(pos), fail = 0;
+                //     }
+                // }
+
+                PoissonDiskSampler<Dim> sampler(halfLengths, center, 2*_radius);
+                std::vector<VectorDr> newPoints = sampler.generatePoints();
+                positions._data.insert(positions._data.end(), newPoints.begin(), newPoints.end());
+            }
+
             velocities._data.resize(positions.size());
         }
 
